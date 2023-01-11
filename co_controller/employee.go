423f8e28d042559bb42c31f9774472511d704468
Supen.Huang <supen.huang@qq.com>
2023-01-11 17:14:52 +0800

    pref 优化控制器逻辑实现

--- co_controller/employee.go
+++ co_controller/employee.go
@@ -3,7 +3,6 @@ package co_controller
 import (
 	"context"
 	"github.com/SupenBysz/gf-admin-community/api_v1"
-	"github.com/SupenBysz/gf-admin-community/sys_model"
 	"github.com/SupenBysz/gf-admin-community/utility/funs"
 	"github.com/SupenBysz/gf-admin-company-modules/api/co_v1"
 	"github.com/SupenBysz/gf-admin-company-modules/co_interface"
@@ -22,53 +21,49 @@ var Employee = func(modules co_interface.IModules) *cEmployee[co_interface.IModu
 }
 
 func (c *cEmployee[T]) GetEmployeeById(ctx context.Context, req *co_v1.GetEmployeeByIdReq) (*co_model.EmployeeRes, error) {
-	return funs.ProxyFunc1[*co_model.EmployeeRes](
-		ctx, req.Id,
-		c.modules.Employee().GetEmployeeById, nil,
+	return funs.CheckPermission(ctx,
+		func() (*co_model.EmployeeRes, error) {
+			ret, err := c.modules.Employee().GetEmployeeById(ctx, req.Id)
+			return (*co_model.EmployeeRes)(ret), err
+		},
 		co_enum.Employee.PermissionType(c.modules).ViewDetail,
 	)
 }
 
 // GetEmployeeDetailById 获取员工详情信息
 func (c *cEmployee[T]) GetEmployeeDetailById(ctx context.Context, req *co_v1.GetEmployeeDetailByIdReq) (res *co_model.EmployeeRes, err error) {
-	return funs.ProxyFunc1[*co_model.EmployeeRes](
-		ctx, req.Id,
-		c.modules.Employee().GetEmployeeDetailById, nil,
+	return funs.CheckPermission(ctx,
+		func() (*co_model.EmployeeRes, error) {
+			ret, err := c.modules.Employee().GetEmployeeDetailById(ctx, req.Id)
+			return (*co_model.EmployeeRes)(ret), err
+		},
 		co_enum.Employee.PermissionType(c.modules).MoreDetail,
 	)
 }
 
 // HasEmployeeByName 员工名称是否存在
 func (c *cEmployee[T]) HasEmployeeByName(ctx context.Context, req *co_v1.HasEmployeeByNameReq) (api_v1.BoolRes, error) {
-	return funs.ProxyFunc[api_v1.BoolRes](
-		ctx,
-		func(ctx context.Context) (api_v1.BoolRes, error) {
+	return funs.CheckPermission(ctx,
+		func() (api_v1.BoolRes, error) {
 			return c.modules.Employee().HasEmployeeByName(ctx, req.Name, req.UnionMainId, req.ExcludeId) == true, nil
-		}, false,
+		},
 	)
 }
 
 // HasEmployeeByNo 员工工号是否存在
 func (c *cEmployee[T]) HasEmployeeByNo(ctx context.Context, req *co_v1.HasEmployeeByNoReq) (api_v1.BoolRes, error) {
-	return funs.ProxyFunc[api_v1.BoolRes](
-		ctx,
-		func(ctx context.Context) (api_v1.BoolRes, error) {
-			return c.modules.Employee().HasEmployeeByName(ctx, req.No, req.UnionMainId, req.ExcludeId) == true, nil
-		}, false,
+	return funs.CheckPermission(ctx,
+		func() (api_v1.BoolRes, error) {
+			return c.modules.Employee().HasEmployeeByNo(ctx, req.No, req.UnionMainId, req.ExcludeId) == true, nil
+		},
 	)
 }
 
 // QueryEmployeeList 查询员工列表
 func (c *cEmployee[T]) QueryEmployeeList(ctx context.Context, req *co_v1.QueryEmployeeListReq) (*co_model.EmployeeListRes, error) {
-	return funs.ProxyFunc1[*co_model.EmployeeListRes](
-		ctx, &req.SearchParams,
-		c.modules.Employee().QueryEmployeeList,
-		&co_model.EmployeeListRes{
-			PaginationRes: sys_model.PaginationRes{
-				Pagination: req.Pagination,
-				PageTotal:  0,
-				Total:      0,
-			},
+	return funs.CheckPermission(ctx,
+		func() (*co_model.EmployeeListRes, error) {
+			return c.modules.Employee().QueryEmployeeList(ctx, &req.SearchParams)
 		},
 		co_enum.Employee.PermissionType(c.modules).List,
 	)
@@ -76,45 +71,55 @@ func (c *cEmployee[T]) QueryEmployeeList(ctx context.Context, req *co_v1.QueryEm
 
 // CreateEmployee 创建员工信息
 func (c *cEmployee[T]) CreateEmployee(ctx context.Context, req *co_v1.CreateEmployeeReq) (*co_model.EmployeeRes, error) {
-	return funs.ProxyFunc1[*co_model.EmployeeRes](
-		ctx, &req.Employee,
-		c.modules.Employee().CreateEmployee, nil,
+	return funs.CheckPermission(ctx,
+		func() (*co_model.EmployeeRes, error) {
+			ret, err := c.modules.Employee().CreateEmployee(ctx, &req.Employee)
+			return (*co_model.EmployeeRes)(ret), err
+		},
 		co_enum.Employee.PermissionType(c.modules).Create,
 	)
 }
 
 // UpdateEmployee 更新员工信息
 func (c *cEmployee[T]) UpdateEmployee(ctx context.Context, req *co_v1.UpdateEmployeeReq) (*co_model.EmployeeRes, error) {
-	return funs.ProxyFunc1[*co_model.EmployeeRes](
-		ctx, &req.Employee,
-		c.modules.Employee().UpdateEmployee, nil,
+	return funs.CheckPermission(ctx,
+		func() (*co_model.EmployeeRes, error) {
+			ret, err := c.modules.Employee().UpdateEmployee(ctx, &req.Employee)
+			return (*co_model.EmployeeRes)(ret), err
+		},
 		co_enum.Employee.PermissionType(c.modules).Update,
 	)
 }
 
 // DeleteEmployee 删除员工信息
 func (c *cEmployee[T]) DeleteEmployee(ctx context.Context, req *co_v1.DeleteEmployeeReq) (api_v1.BoolRes, error) {
-	return funs.ProxyFunc1[api_v1.BoolRes](
-		ctx, req.Id,
-		c.modules.Employee().DeleteEmployee, false,
+	return funs.CheckPermission(ctx,
+		func() (api_v1.BoolRes, error) {
+			ret, err := c.modules.Employee().DeleteEmployee(ctx, req.Id)
+			return ret == true, err
+		},
 		co_enum.Employee.PermissionType(c.modules).Delete,
 	)
 }
 
 // SetEmployeeMobile 设置员工手机号
 func (c *cEmployee[T]) SetEmployeeMobile(ctx context.Context, req *co_v1.SetEmployeeMobileReq) (api_v1.BoolRes, error) {
-	return funs.ProxyFunc2[api_v1.BoolRes](
-		ctx, req.Mobile, req.Captcha,
-		c.modules.Employee().SetEmployeeMobile, false,
+	return funs.CheckPermission(ctx,
+		func() (api_v1.BoolRes, error) {
+			ret, err := c.modules.Employee().SetEmployeeMobile(ctx, req.Mobile, req.Captcha)
+			return ret == true, err
+		},
 		co_enum.Employee.PermissionType(c.modules).SetMobile,
 	)
 }
 
 // SetEmployeeAvatar 设置员工头像
 func (c *cEmployee[T]) SetEmployeeAvatar(ctx context.Context, req *co_v1.SetEmployeeAvatarReq) (api_v1.BoolRes, error) {
-	return funs.ProxyFunc1[api_v1.BoolRes](
-		ctx, req.ImageId,
-		c.modules.Employee().SetEmployeeAvatar, false,
+	return funs.CheckPermission(ctx,
+		func() (api_v1.BoolRes, error) {
+			ret, err := c.modules.Employee().SetEmployeeAvatar(ctx, req.ImageId)
+			return ret == true, err
+		},
 		co_enum.Employee.PermissionType(c.modules).SetAvatar,
 	)
 }
--- co_controller/team.go
+++ co_controller/team.go
@@ -3,7 +3,6 @@ package co_controller
 import (
 	"context"
 	"github.com/SupenBysz/gf-admin-community/api_v1"
-	"github.com/SupenBysz/gf-admin-community/sys_model"
 	"github.com/SupenBysz/gf-admin-community/utility/funs"
 	"github.com/SupenBysz/gf-admin-company-modules/api/co_v1"
 	"github.com/SupenBysz/gf-admin-company-modules/co_interface"
@@ -22,115 +21,99 @@ var Team = func(modules co_interface.IModules) *cTeam[co_interface.IModules] {
 }
 
 func (c *cTeam[T]) GetTeamById(ctx context.Context, req *co_v1.GetTeamByIdReq) (*co_model.TeamRes, error) {
-	return funs.ProxyFunc1[*co_model.TeamRes](
-		ctx, req.Id,
-		c.modules.Team().GetTeamById, nil,
+	return funs.CheckPermission(ctx,
+		func() (*co_model.TeamRes, error) {
+			ret, err := c.modules.Team().GetTeamById(ctx, req.Id)
+			return (*co_model.TeamRes)(ret), err
+		},
 		co_enum.Team.PermissionType(c.modules).ViewDetail,
 	)
 }
 func (c *cTeam[T]) HasTeamByName(ctx context.Context, req *co_v1.HasTeamByNameReq) (api_v1.BoolRes, error) {
-	return funs.ProxyFunc[api_v1.BoolRes](
-		ctx,
-		func(ctx context.Context) (api_v1.BoolRes, error) {
+	return funs.CheckPermission(ctx,
+		func() (api_v1.BoolRes, error) {
 			return c.modules.Team().HasTeamByName(ctx, req.Name, req.UnionMainId, req.ExcludeId) == true, nil
-		}, false,
+		},
 	)
 }
 
 func (c *cTeam[T]) QueryTeamList(ctx context.Context, req *co_v1.QueryTeamListReq) (*co_model.TeamListRes, error) {
-	return funs.ProxyFunc1[*co_model.TeamListRes](
-		ctx, &req.SearchParams,
-		c.modules.Team().QueryTeamList,
-		&co_model.TeamListRes{
-			PaginationRes: sys_model.PaginationRes{
-				Pagination: req.Pagination,
-				PageTotal:  0,
-				Total:      0,
-			},
+	return funs.CheckPermission(ctx,
+		func() (*co_model.TeamListRes, error) {
+			return c.modules.Team().QueryTeamList(ctx, &req.SearchParams)
 		},
 		co_enum.Team.PermissionType(c.modules).List,
 	)
 }
 
 func (c *cTeam[T]) CreateTeam(ctx context.Context, req *co_v1.CreateTeamReq) (*co_model.TeamRes, error) {
-	return funs.ProxyFunc1[*co_model.TeamRes](
-		ctx, &req.Team,
-		c.modules.Team().CreateTeam,
-		nil,
+	return funs.CheckPermission(ctx,
+		func() (*co_model.TeamRes, error) {
+			ret, err := c.modules.Team().CreateTeam(ctx, &req.Team)
+			return (*co_model.TeamRes)(ret), err
+		},
 		co_enum.Team.PermissionType(c.modules).Create,
 	)
 }
 
 func (c *cTeam[T]) UpdateTeam(ctx context.Context, req *co_v1.UpdateTeamReq) (*co_model.TeamRes, error) {
-	return funs.ProxyFunc3[*co_model.TeamRes](ctx,
-		req.Id, req.Name, req.Remark,
-		c.modules.Team().UpdateTeam, nil,
+	return funs.CheckPermission(ctx,
+		func() (*co_model.TeamRes, error) {
+			ret, err := c.modules.Team().UpdateTeam(ctx, req.Id, req.Name, req.Remark)
+			return (*co_model.TeamRes)(ret), err
+		},
 		co_enum.Team.PermissionType(c.modules).Update,
 	)
 }
 
 func (c *cTeam[T]) DeleteTeam(ctx context.Context, req *co_v1.DeleteTeamReq) (api_v1.BoolRes, error) {
-	return funs.ProxyFunc1[api_v1.BoolRes](
-		ctx, req.Id,
-		c.modules.Team().DeleteTeam, false,
+	return funs.CheckPermission(ctx,
+		func() (api_v1.BoolRes, error) {
+			return c.modules.Team().DeleteTeam(ctx, req.Id)
+		},
 		co_enum.Team.PermissionType(c.modules).Delete,
 	)
 }
 
 func (c *cTeam[T]) GetTeamMemberList(ctx context.Context, req *co_v1.GetTeamMemberListReq) (*co_model.EmployeeListRes, error) {
-	return funs.ProxyFunc1[*co_model.EmployeeListRes](
-		ctx, req.Id,
-		c.modules.Team().GetTeamMemberList,
-		&co_model.EmployeeListRes{
-			PaginationRes: sys_model.PaginationRes{
-				Pagination: sys_model.Pagination{
-					Page:     1,
-					PageSize: 100,
-				},
-				PageTotal: 0,
-				Total:     0,
-			},
+	return funs.CheckPermission(ctx,
+		func() (*co_model.EmployeeListRes, error) {
+			return c.modules.Team().GetTeamMemberList(ctx, req.Id)
 		},
 		co_enum.Team.PermissionType(c.modules).MemberDetail,
 	)
 }
 
 func (c *cTeam[T]) QueryTeamByEmployeeList(ctx context.Context, req *co_v1.QueryTeamByEmployeeListReq) (*co_model.TeamListRes, error) {
-	return funs.ProxyFunc2[*co_model.TeamListRes](
-		ctx, req.EmployeeId, req.UnionMainId,
-		c.modules.Team().QueryTeamByEmployeeList,
-		&co_model.TeamListRes{
-			PaginationRes: sys_model.PaginationRes{
-				Pagination: sys_model.Pagination{
-					Page:     1,
-					PageSize: 100,
-				},
-				PageTotal: 0,
-				Total:     0,
-			},
+	return funs.CheckPermission(ctx,
+		func() (*co_model.TeamListRes, error) {
+			return c.modules.Team().QueryTeamByEmployeeList(ctx, req.EmployeeId, req.UnionMainId)
 		},
 		co_enum.Team.PermissionType(c.modules).List,
 	)
 }
 
 func (c *cTeam[T]) SetTeamMember(ctx context.Context, req *co_v1.SetTeamMemberReq) (api_v1.BoolRes, error) {
-	return funs.ProxyFunc2[api_v1.BoolRes](
-		ctx, req.Id, req.EmployeeIds,
-		c.modules.Team().SetTeamMember, false,
+	return funs.CheckPermission(ctx,
+		func() (api_v1.BoolRes, error) {
+			return c.modules.Team().SetTeamMember(ctx, req.Id, req.EmployeeIds)
+		},
 		co_enum.Team.PermissionType(c.modules).SetMember,
 	)
 }
 func (c *cTeam[T]) SetTeamOwner(ctx context.Context, req *co_v1.SetTeamOwnerReq) (api_v1.BoolRes, error) {
-	return funs.ProxyFunc2[api_v1.BoolRes](
-		ctx, req.Id, req.EmployeeId,
-		c.modules.Team().SetTeamOwner, false,
-		co_enum.Team.PermissionType(c.modules).SetOwner,
+	return funs.CheckPermission(ctx,
+		func() (api_v1.BoolRes, error) {
+			return c.modules.Team().SetTeamOwner(ctx, req.Id, req.EmployeeId)
+		},
+		co_enum.Team.PermissionType(c.modules).SetCaptain,
 	)
 }
 func (c *cTeam[T]) SetTeamCaptain(ctx context.Context, req *co_v1.SetTeamCaptainReq) (api_v1.BoolRes, error) {
-	return funs.ProxyFunc2[api_v1.BoolRes](
-		ctx, req.Id, req.EmployeeId,
-		c.modules.Team().SetTeamCaptain, false,
+	return funs.CheckPermission(ctx,
+		func() (api_v1.BoolRes, error) {
+			return c.modules.Team().SetTeamCaptain(ctx, req.Id, req.EmployeeId)
+		},
 		co_enum.Team.PermissionType(c.modules).SetCaptain,
 	)
 }
