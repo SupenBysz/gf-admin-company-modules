cbfe598b5f6a4ad16c3c5df17116af386d5e2c88
LinFeiFei <2778368047@qq.com>
2023-01-16 22:49:07 +0800

    feat: 添加控制器接口及对应的映射方法

---
+++ co_interface/i_controller/company.go
@@ -0,0 +1,22 @@
+package i_controller
+
+import (
+	"context"
+	"github.com/SupenBysz/gf-admin-community/api_v1"
+	"github.com/SupenBysz/gf-admin-company-modules/api/co_company_api"
+	"github.com/SupenBysz/gf-admin-company-modules/co_model"
+)
+
+type ICompany interface {
+	iModule
+	// GetCompanyById 通过ID获取公司信息
+	GetCompanyById(ctx context.Context, req *co_company_api.GetCompanyByIdReq) (*co_model.CompanyRes, error)
+	// HasCompanyByName 公司名称是否存在
+	HasCompanyByName(ctx context.Context, req *co_company_api.HasCompanyByNameReq) (api_v1.BoolRes, error)
+	// QueryCompanyList 查询公司列表
+	QueryCompanyList(ctx context.Context, req *co_company_api.QueryCompanyListReq) (*co_model.CompanyListRes, error)
+	// CreateCompany 创建公司信息
+	CreateCompany(ctx context.Context, req *co_company_api.CreateCompanyReq) (*co_model.CompanyRes, error)
+	// UpdateCompany 更新公司信息
+	UpdateCompany(ctx context.Context, req *co_company_api.UpdateCompanyReq) (*co_model.CompanyRes, error)
+}
---
+++ co_interface/i_controller/employee.go
@@ -0,0 +1,41 @@
+package i_controller
+
+import (
+	"context"
+	"github.com/SupenBysz/gf-admin-community/api_v1"
+	"github.com/SupenBysz/gf-admin-company-modules/api/co_company_api"
+	"github.com/SupenBysz/gf-admin-company-modules/co_model"
+)
+
+type IEmployee interface {
+	iModule
+	// GetEmployeeById 根据id获取员工信息
+	GetEmployeeById(ctx context.Context, req *co_company_api.GetEmployeeByIdReq) (*co_model.EmployeeRes, error)
+
+	// GetEmployeeDetailById 获取员工详情信息
+	GetEmployeeDetailById(ctx context.Context, req *co_company_api.GetEmployeeDetailByIdReq) (res *co_model.EmployeeRes, err error)
+
+	// HasEmployeeByName 员工名称是否存在
+	HasEmployeeByName(ctx context.Context, req *co_company_api.HasEmployeeByNameReq) (api_v1.BoolRes, error)
+
+	// HasEmployeeByNo 员工工号是否存在
+	HasEmployeeByNo(ctx context.Context, req *co_company_api.HasEmployeeByNoReq) (api_v1.BoolRes, error)
+
+	// QueryEmployeeList 查询员工列表
+	QueryEmployeeList(ctx context.Context, req *co_company_api.QueryEmployeeListReq) (*co_model.EmployeeListRes, error)
+
+	// CreateEmployee 创建员工信息
+	CreateEmployee(ctx context.Context, req *co_company_api.CreateEmployeeReq) (*co_model.EmployeeRes, error)
+
+	// UpdateEmployee 更新员工信息
+	UpdateEmployee(ctx context.Context, req *co_company_api.UpdateEmployeeReq) (*co_model.EmployeeRes, error)
+
+	// DeleteEmployee 删除员工信息
+	DeleteEmployee(ctx context.Context, req *co_company_api.DeleteEmployeeReq) (api_v1.BoolRes, error)
+
+	// SetEmployeeMobile 设置员工手机号
+	SetEmployeeMobile(ctx context.Context, req *co_company_api.SetEmployeeMobileReq) (api_v1.BoolRes, error)
+
+	// SetEmployeeAvatar 设置员工头像
+	SetEmployeeAvatar(ctx context.Context, req *co_company_api.SetEmployeeAvatarReq) (api_v1.BoolRes, error)
+}
