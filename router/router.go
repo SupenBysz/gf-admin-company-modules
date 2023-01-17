4e021d7fb8392a5a99f4989eb55ed199970d18d2
SupenBysz <supen.huang@qq.com>
2023-01-17 10:00:57 +0800

    pref 增加模块默认路由

---
+++ router/router.go
@@ -0,0 +1,69 @@
+package router
+
+import (
+	"github.com/SupenBysz/gf-admin-company-modules/co_interface"
+	"github.com/SupenBysz/gf-admin-company-modules/example/internal/consts"
+	"github.com/gogf/gf/v2/net/ghttp"
+	"github.com/gogf/gf/v2/text/gstr"
+)
+
+func ModulesGroup(modules co_interface.IModules, group *ghttp.RouterGroup) *ghttp.RouterGroup {
+	CompanyGroup(modules, group)
+	EmployeeGroup(modules, group)
+	TeamGroup(modules, group)
+	MyGroup(modules, group)
+	return group
+}
+
+func CompanyGroup(modules co_interface.IModules, group *ghttp.RouterGroup) *ghttp.RouterGroup {
+	routePrefix := modules.GetConfig().RoutePrefix + "/" + gstr.LcFirst(modules.GetConfig().Identifier.Company)
+	controller := consts.Global.Controller.Company
+	group.POST(routePrefix+"/createCompany", controller.CreateCompany)
+	group.POST(routePrefix+"/updateCompany", controller.UpdateCompany)
+	group.POST(routePrefix+"/hasCompanyByName", controller.HasCompanyByName)
+	group.POST(routePrefix+"/getCompanyById", controller.GetCompanyById)
+	group.POST(routePrefix+"/queryCompanyList", controller.QueryCompanyList)
+	return group
+}
+
+func EmployeeGroup(modules co_interface.IModules, group *ghttp.RouterGroup) *ghttp.RouterGroup {
+	routePrefix := modules.GetConfig().RoutePrefix + "/" + gstr.LcFirst(modules.GetConfig().Identifier.Employee)
+	controller := consts.Global.Controller.Employee
+	group.POST(routePrefix+"/getEmployeeById", controller.GetEmployeeById)
+	group.POST(routePrefix+"/getEmployeeDetailById", controller.GetEmployeeDetailById)
+	group.POST(routePrefix+"/hasEmployeeByName", controller.HasEmployeeByName)
+	group.POST(routePrefix+"/hasEmployeeByNo", controller.HasEmployeeByNo)
+	group.POST(routePrefix+"/queryEmployeeList", controller.QueryEmployeeList)
+	group.POST(routePrefix+"/createEmployee", controller.CreateEmployee)
+	group.POST(routePrefix+"/updateEmployee", controller.UpdateEmployee)
+	group.POST(routePrefix+"/deleteEmployee", controller.DeleteEmployee)
+	group.POST(routePrefix+"/setEmployeeMobile", controller.SetEmployeeMobile)
+	group.POST(routePrefix+"/setEmployeeAvatar", controller.SetEmployeeAvatar)
+	return group
+}
+
+func TeamGroup(modules co_interface.IModules, group *ghttp.RouterGroup) *ghttp.RouterGroup {
+	routePrefix := modules.GetConfig().RoutePrefix + "/" + gstr.LcFirst(modules.GetConfig().Identifier.Team)
+	controller := consts.Global.Controller.Team
+	group.POST(routePrefix+"/getTeamById", controller.GetTeamById)
+	group.POST(routePrefix+"/hasTeamByName", controller.HasTeamByName)
+	group.POST(routePrefix+"/queryTeamList", controller.QueryTeamList)
+	group.POST(routePrefix+"/createTeam", controller.CreateTeam)
+	group.POST(routePrefix+"/updateTeam", controller.UpdateTeam)
+	group.POST(routePrefix+"/getTeamMemberList", controller.GetTeamMemberList)
+	group.POST(routePrefix+"/queryTeamListByEmployee", controller.QueryTeamListByEmployee)
+	group.POST(routePrefix+"/setTeamMember", controller.SetTeamMember)
+	group.POST(routePrefix+"/setTeamOwner", controller.SetTeamOwner)
+	group.POST(routePrefix+"/setTeamCaptain", controller.SetTeamCaptain)
+	group.POST(routePrefix+"/deleteTeam", controller.DeleteTeam)
+	return group
+}
+
+func MyGroup(modules co_interface.IModules, group *ghttp.RouterGroup) *ghttp.RouterGroup {
+	controller := consts.Global.Controller.My
+	routePrefix := modules.GetConfig().RoutePrefix + "/my"
+	group.POST(routePrefix+"/getProfile", controller.GetProfile)
+	group.POST(routePrefix+"/getCompany", controller.GetCompany)
+	group.POST(routePrefix+"/getTeams", controller.GetTeams)
+	return group
+}
