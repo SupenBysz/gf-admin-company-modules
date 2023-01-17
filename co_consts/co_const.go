5cd8e3c9e6193cb7edf5e95f742992be85f29b0e
SupenBysz <supen.huang@qq.com>
2023-01-18 05:48:00 +0800

    pref 优化初始化流程

---
+++ co_consts/co_const.go
@@ -0,0 +1,7 @@
+package co_consts
+
+import "github.com/SupenBysz/gf-admin-community/utility/permission"
+
+var (
+	PermissionTree []*permission.SysPermissionTree
+)
--- co_module/modules.go
+++ co_module/modules.go
@@ -2,11 +2,14 @@ package co_module
 
 import (
 	"context"
+	"github.com/SupenBysz/gf-admin-company-modules/co_consts"
 	"github.com/SupenBysz/gf-admin-company-modules/co_interface"
 	"github.com/SupenBysz/gf-admin-company-modules/co_model"
 	"github.com/SupenBysz/gf-admin-company-modules/co_model/co_dao"
+	"github.com/SupenBysz/gf-admin-company-modules/internal/boot"
 	"github.com/SupenBysz/gf-admin-company-modules/internal/logic/company"
 	"github.com/gogf/gf/v2/i18n/gi18n"
+	"github.com/gogf/gf/v2/text/gstr"
 )
 
 type Modules struct {
@@ -52,7 +55,7 @@ func (m *Modules) SetI18n(i18n *gi18n.Manager) error {
 	if i18n == nil {
 		i18n = gi18n.New()
 		i18n.SetLanguage("zh-CN")
-		err := i18n.SetPath("i18n/" + m.conf.KeyIndex)
+		err := i18n.SetPath("i18n/" + gstr.ToLower(m.conf.KeyIndex))
 		if err != nil {
 			return err
 		}
@@ -71,10 +74,16 @@ func NewModules(
 		xDao: xDao,
 	}
 
+	// 初始化默认多语言对象
+	module.SetI18n(nil)
+
 	module.company = company.NewCompany(module, module.xDao)
 	module.employee = company.NewEmployee(module, module.xDao)
 	module.team = company.NewTeam(module, module.xDao)
 	module.my = company.NewMy(module, module.xDao)
+
+	// 权限树追加权限
+	co_consts.PermissionTree = append(co_consts.PermissionTree, boot.InitPermission(module)...)
 
 	return module
 }
