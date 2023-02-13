cd4314df703413beb41cd9173cfa69c8cad18282
0852FeiFeiLin <2778368047@qq.com>
2023-02-13 18:33:09 +0800

    feat: 添加hook层模型定义
    - 将和hook相关定义抽取出来，避免出现包循环引用问题

---
+++ co_model/co_hook/co_hook.go
@@ -0,0 +1,8 @@
+package internal
+
+import "github.com/SupenBysz/gf-admin-company-modules/co_model/co_hook/internal"
+
+type (
+	FinancialInOutType internal.InOutTypeEnum
+	FinancialTradeType internal.TradeTypeEnum
+)
