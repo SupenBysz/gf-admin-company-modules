c2c1062534fab64d9fd38b6b226446236b38cc69
0852FeiFeiLin <2778368047@qq.com>
2023-03-29 01:23:52 +0800

    feat: 添加默认的货币单位

--- co_consts/co_const.go
+++ co_consts/co_const.go
@@ -1,11 +1,25 @@
 package co_consts
 
 import (
+	"context"
 	"github.com/SupenBysz/gf-admin-community/sys_model"
+	"github.com/gogf/gf/v2/frame/g"
 )
 
+type global struct {
+	// 默认货币单位
+	DefaultCurrency string
+}
+
 var (
+	Global = global{}
+
 	PermissionTree []*sys_model.SysPermissionTree
 
 	FinancialPermissionTree []*sys_model.SysPermissionTree
 )
+
+func init() {
+	defaultCurrency, _ := g.Cfg().Get(context.Background(), "service.defaultCurrency")
+	Global.DefaultCurrency = defaultCurrency.String()
+}
