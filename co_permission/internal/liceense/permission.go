e10328ebafd3d8aa91d0fbc898c122f6f9b40fb6
Supen.Huang <supen.huang@qq.com>
2023-11-20 20:41:07 +0800

    perf: 适配基础骨架新版本，并更新 golang 版本至 1.21+

--- co_permission/internal/liceense/permission.go
+++ co_permission/internal/liceense/permission.go
@@ -1,6 +1,7 @@
 package liceense
 
 import (
+	"github.com/SupenBysz/gf-admin-community/utility/permission"
 	"github.com/gogf/gf/v2/container/gmap"
 	"github.com/gogf/gf/v2/util/gconv"
 	"github.com/kysion/base-library/utility/base_permission"
@@ -18,11 +19,11 @@ type permissionType struct {
 
 var (
 	PermissionType = permissionType{
-		ViewDetail: base_permission.NewInIdentifier("ViewDetail", "查看主体资质信息", "查看某条主体资质信息"),
-		List:       base_permission.NewInIdentifier("List", "主体资质列表", "查看所有主体资质信息"),
-		Update:     base_permission.NewInIdentifier("Update", "更新资质审核信息", "更新某条资质审核信息"),
-		SetState:   base_permission.NewInIdentifier("SetState", "设置主体资质状态", "设置某主体资质认证状态"),
-		Create:     base_permission.NewInIdentifier("Create", "创建主体资质", "创建主体资质信息"),
+		ViewDetail: permission.NewInIdentifier("ViewDetail", "查看主体资质信息", "查看某条主体资质信息"),
+		List:       permission.NewInIdentifier("List", "主体资质列表", "查看所有主体资质信息"),
+		Update:     permission.NewInIdentifier("Update", "更新资质审核信息", "更新某条资质审核信息"),
+		SetState:   permission.NewInIdentifier("SetState", "设置主体资质状态", "设置某主体资质认证状态"),
+		Create:     permission.NewInIdentifier("Create", "创建主体资质", "创建主体资质信息"),
 	}
 	permissionTypeMap = gmap.NewStrAnyMapFrom(gconv.Map(PermissionType))
 )
