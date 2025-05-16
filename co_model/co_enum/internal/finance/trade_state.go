8d43845a26f228cd7e9bacce229a6afbf5c97521
SupenBysz <supen.huang@qq.com>
2025-05-16 19:52:17 +0800

    feat(finance): 新增交易状态枚举及相关逻辑
    - 在 co_enum.go 文件中新增 FinanceTradeState 枚举，表示不同的交易状态。
    - 更新 finance/index.go 文件，添加 TradeState 字段以支持交易状态的管理。
    - 新增 trade_state.go 文件，定义交易状态的具体值及其描述，提升代码的可读性和可维护性。

---
+++ co_model/co_enum/internal/finance/trade_state.go
@@ -0,0 +1,62 @@
+package finance
+
+import "github.com/kysion/base-library/utility/enum"
+
+// TradeStateEnum 交易状态：1待支付、2支付中、4已支付、8取消支付、16交易完成、32退款中、64已退款、128支付超时、256已关闭
+type TradeStateEnum enum.IEnumCode[int]
+
+type tradeState struct {
+	WaitPay        TradeStateEnum
+	Paying         TradeStateEnum
+	Paid           TradeStateEnum
+	Cancel         TradeStateEnum
+	Completed      TradeStateEnum
+	Refunding      TradeStateEnum
+	Refunded       TradeStateEnum
+	PaymentTimeout TradeStateEnum
+	Closed         TradeStateEnum
+}
+
+var TradeState = tradeState{
+	WaitPay:        enum.New[TradeStateEnum](1, "待支付"),
+	Paying:         enum.New[TradeStateEnum](2, "支付中"),
+	Paid:           enum.New[TradeStateEnum](4, "已支付"),
+	Cancel:         enum.New[TradeStateEnum](8, "取消支付"),
+	Completed:      enum.New[TradeStateEnum](16, "交易完成"),
+	Refunding:      enum.New[TradeStateEnum](32, "退款中"),
+	Refunded:       enum.New[TradeStateEnum](64, "已退款"),
+	PaymentTimeout: enum.New[TradeStateEnum](128, "支付超时"),
+	Closed:         enum.New[TradeStateEnum](256, "已关闭"),
+}
+
+func (e tradeState) New(code int, description string) TradeStateEnum {
+	if code == TradeState.WaitPay.Code() {
+		return TradeState.WaitPay
+	}
+	if code == TradeState.Paying.Code() {
+		return TradeState.Paying
+	}
+	if code == TradeState.Paid.Code() {
+		return TradeState.Paid
+	}
+	if code == TradeState.Cancel.Code() {
+		return TradeState.Cancel
+	}
+	if code == TradeState.Completed.Code() {
+		return TradeState.Completed
+	}
+	if code == TradeState.Refunding.Code() {
+		return TradeState.Refunding
+	}
+	if code == TradeState.Refunded.Code() {
+		return TradeState.Refunded
+	}
+	if code == TradeState.PaymentTimeout.Code() {
+		return TradeState.PaymentTimeout
+	}
+	if code == TradeState.Closed.Code() {
+		return TradeState.Closed
+	}
+
+	return enum.New[TradeStateEnum](code, description)
+}
